1ee0ef9b6760f74fd391f644547fb5993f8995d4
Mariah <mariahschock@gmail.com>
2022-06-10 15:38:42 -0700

    playing with npm install

--- src/Music.js
+++ src/Music.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import useSound from 'use-sound';
 import Circus from './Circus.mp3';
+import './App.css';
 
 export default function Music() {
   const [play] = useSound(Circus, { interrupt: true });
   return (
-    <div>
+    <div className="music">
       <button onClick={play}>Play Circus Music</button>
     </div>
   );
